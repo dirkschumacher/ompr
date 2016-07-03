8cf4159cfee9801556fd1080df92bf053a414dd2
Dirk Schumacher <mail@dirk-schumacher.net>
2016-07-03 11:09:25 +0200

    Add tests for exported function
    That was previously an internal helper function for the solvers. Now it is public.

--- tests/testthat/test-helpers.R
+++ tests/testthat/test-helpers.R
@@ -15,3 +15,17 @@ test_that("sum_exp does not evaluate other variables", {
   result <- sum_exp(weights[i] * x[i], i = 1:n, free_vars = "x")
   expect_equal(deparse(result), "weights[1L] * x[1L] + weights[2L] * x[2L] + weights[3L] * x[3L]")
 })
+
+test_that("extract_coefficients can extract constant", {
+  ast <- substitute(1 + x * 10 + x[1, 2] + 23 + -1 * 3)
+  result <- extract_coefficients(ast)
+  expect_equivalent(21, result$constant)
+})
+
+test_that("extract_coefficients can extract coefficients", {
+  ast <- substitute(1 + x * (5 + 5) + x[1, 2] + 23 + -1 * 3)
+  result <- extract_coefficients(ast)
+  expect_equal(2, length(result$coefficients))
+  expect_equivalent(10, result$coefficients[["x"]]$coef)
+  expect_equivalent(1, result$coefficients[["x[1, 2]"]]$coef)
+})
