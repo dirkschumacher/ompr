531f41c3b8421cd4bab001733f6a200a2e7bc901
Dirk Schumacher <mail@dirk-schumacher.net>
2016-07-09 11:34:03 +0200

    Exclude linter for objective function class

--- R/class-objective-function.R
+++ R/class-objective-function.R
@@ -4,7 +4,8 @@
 #' @slot original_expression the original expression as supplied by the user
 #' @slot direction the direction of optimization
 #' @export
-ObjectiveFunction <- setClass("ObjectiveFunction",
+
+ObjectiveFunction <- setClass("ObjectiveFunction", # Exclude Linting
                               slots = c(
                                 expression = "expression",
                                 original_expression = "expression",
