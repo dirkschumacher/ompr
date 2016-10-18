020618881531015198b952ea094403177c15dbf5
Dirk Schumacher <mail@dirk-schumacher.net>
2016-10-18 23:42:51 +0200

    Make is_non_linear imperative
    I fear the stack overflow error

--- src/is_non_linear_impl.cpp
+++ src/is_non_linear_impl.cpp
@@ -1,21 +1,30 @@
 #include <Rcpp.h>
 #include <algorithm>
+#include <stack>
 
 using namespace Rcpp;
 
 // checks if an AST contains a variable
 bool contains_var(CharacterVector var_names, SEXP x) {
-  int x_type = TYPEOF(x);
-  if (x_type == 1) { // name
-    Rcpp::CharacterVector el(x);
-    return std::find(var_names.begin(), var_names.end(), el[0]) != var_names.end();
-  } else if (x_type == 6) { // call
-    Rcpp::Language ast(x);
-    for(int i = 1; i < ast.size(); i++) {
-      SEXP sub_ast = ast[i].get();
-      int sub_ast_type = TYPEOF(sub_ast);
-      if (contains_var(var_names, sub_ast)) {
+  std::stack<SEXP> stack;
+  stack.push(x);
+  SEXP local_obj;
+  SEXP sub_ast;
+  int local_obj_type;
+  while(!stack.empty()) {
+    local_obj = stack.top();
+    stack.pop();
+    local_obj_type = TYPEOF(local_obj);
+    if (local_obj_type == 1) { // name
+      Rcpp::CharacterVector el(local_obj);
+      if (std::find(var_names.begin(), var_names.end(), el[0]) != var_names.end()) {
         return true;
+      }
+    } else if (local_obj_type == 6) { // call
+      Rcpp::Language ast(local_obj);
+      for(int i = 1; i < ast.size(); i++) {
+        sub_ast = ast[i].get();
+        stack.push(sub_ast);
       }
     }
   }
@@ -25,25 +34,31 @@ bool contains_var(CharacterVector var_names, SEXP x) {
 // checks if an AST is non-linear
 // [[Rcpp::export]]
 bool is_non_linear_impl(CharacterVector var_names, SEXP x) {
-  int x_type = TYPEOF(x);
-  if (x_type == 6) { // call
-    Rcpp::Language ast(x);
-    if (ast.size() == 3) {
-      Rcpp::CharacterVector op(ast[0]);
-      std::string op_string = Rcpp::as<std::string>(op);
-      if (op_string == "*" || op_string == "/" || op_string == "^") {
-        bool non_linear = contains_var(var_names, ast[1]) &&
-          contains_var(var_names, ast[2]);
-        if (non_linear) {
-          return true;
+  std::stack<SEXP> stack;
+  stack.push(x);
+  SEXP local_obj;
+  SEXP sub_ast;
+  int local_obj_type;
+  while(!stack.empty()) {
+    local_obj = stack.top();
+    stack.pop();
+    local_obj_type = TYPEOF(local_obj);
+    if (local_obj_type == 6) { // call
+      Rcpp::Language ast(local_obj);
+      if (ast.size() == 3) {
+        Rcpp::CharacterVector op(ast[0]);
+        std::string op_string = Rcpp::as<std::string>(op);
+        if (op_string == "*" || op_string == "/" || op_string == "^") {
+          bool non_linear = contains_var(var_names, ast[1]) &&
+            contains_var(var_names, ast[2]);
+          if (non_linear) {
+            return true;
+          }
         }
       }
-    }
-    for(int i = 1; i < ast.size(); i++) {
-      SEXP sub_ast = ast[i].get();
-      int sub_ast_type = TYPEOF(sub_ast);
-      if (is_non_linear_impl(var_names, sub_ast)) {
-        return true;
+      for(int i = 1; i < ast.size(); i++) {
+        sub_ast = ast[i].get();
+        stack.push(sub_ast);
       }
     }
   }
