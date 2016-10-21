da457bb538418b10e4abc47322fbca3e641794ef
Dirk Schumacher <mail@dirk-schumacher.net>
2016-10-21 22:34:06 +0200

    Reformat c++ code

--- src/is_non_linear_impl.cpp
+++ src/is_non_linear_impl.cpp
@@ -8,18 +8,19 @@ using namespace Rcpp;
 bool contains_var(CharacterVector var_names, SEXP x) {
   std::stack<SEXP> stack;
   stack.push(x);
-  while(!stack.empty()) {
+  while (!stack.empty()) {
     SEXP local_obj = stack.top();
     stack.pop();
     int local_obj_type = TYPEOF(local_obj);
     if (local_obj_type == SYMSXP) { // name
       CharacterVector el(local_obj);
-      if (std::find(var_names.begin(), var_names.end(), el[0]) != var_names.end()) {
+      if (std::find(var_names.begin(), var_names.end(), el[0]) !=
+          var_names.end()) {
         return true;
       }
     } else if (local_obj_type == LANGSXP) { // call
       Language ast(local_obj);
-      for(int i = 1; i < ast.size(); i++) {
+      for (int i = 1; i < ast.size(); i++) {
         stack.push(ast[i].get());
       }
     }
@@ -32,7 +33,7 @@ bool contains_var(CharacterVector var_names, SEXP x) {
 bool is_non_linear_impl(CharacterVector var_names, SEXP x) {
   std::stack<SEXP> stack;
   stack.push(x);
-  while(!stack.empty()) {
+  while (!stack.empty()) {
     SEXP local_obj = stack.top();
     stack.pop();
     int local_obj_type = TYPEOF(local_obj);
@@ -43,20 +44,19 @@ bool is_non_linear_impl(CharacterVector var_names, SEXP x) {
         std::string op_string = as<std::string>(op);
         if (op_string == "*" || op_string == "/" || op_string == "^") {
           bool non_linear = contains_var(var_names, ast[1]) &&
-            contains_var(var_names, ast[2]);
+                            contains_var(var_names, ast[2]);
           if (non_linear) {
             return true;
           }
         }
       }
-      for(int i = 1; i < ast.size(); i++) {
+      for (int i = 1; i < ast.size(); i++) {
         stack.push(ast[i].get());
       }
     }
   }
   return false;
 }
-
 
 // You can include R code blocks in C++ files processed with sourceCpp
 // (useful for testing and development). The R code will be automatically
