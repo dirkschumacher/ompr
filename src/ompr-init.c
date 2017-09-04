527fedcdc758defe34616eb1ccf1823f11934205
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-09-04 21:02:45 +0100

    Fix interface with recent Rcpp
    It looks like things changed with Rcpp 0.12.12 - see news items about
    underscores and issues #722 and #721.  This might warrant an Rcpp minimum
    version of 0.12.12 but practically that affects you only when you are using
    compileAttributes and not calling the initialisation skeleton code.

--- src/ompr-init.c
+++ src/ompr-init.c
@@ -6,12 +6,12 @@
 #include <stdlib.h> // for NULL
 #include <R_ext/Rdynload.h>
 
-extern SEXP ompr_check_for_unknown_vars_impl(SEXP, SEXP);
-extern SEXP ompr_is_non_linear_impl(SEXP, SEXP);
+extern SEXP _ompr_check_for_unknown_vars_impl(SEXP, SEXP);
+extern SEXP _ompr_is_non_linear_impl(SEXP, SEXP);
 
 static const R_CallMethodDef CallEntries[] = {
-    {"ompr_check_for_unknown_vars_impl", (DL_FUNC) &ompr_check_for_unknown_vars_impl, 2},
-    {"ompr_is_non_linear_impl",          (DL_FUNC) &ompr_is_non_linear_impl,          2},
+    {"_ompr_check_for_unknown_vars_impl", (DL_FUNC) &_ompr_check_for_unknown_vars_impl, 2},
+    {"_ompr_is_non_linear_impl",          (DL_FUNC) &_ompr_is_non_linear_impl,          2},
     {NULL, NULL, 0}
 };
 
