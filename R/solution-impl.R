219351014d4f86d4d55f9c304173b7c650d95be6
Dirk Schumacher <mail@dirk-schumacher.net>
2022-01-10 17:54:33 +0100

    Update docs

--- R/solution-impl.R
+++ R/solution-impl.R
@@ -66,7 +66,7 @@ extract_solution <- function(model, solution_vector, expr) {
       na_rows <- as.logical(apply(is.na(var_index), 1, all))
       var_index <- var_index[!na_rows, , drop = FALSE]
       var_values <- solution_vector[grepl(solution_names,
-        pattern = instance_pattern
+                                          pattern = instance_pattern
       )]
       result_df <- as.data.frame(var_index[, seq_len(ncol(var_index))[-1]])
       for (x in colnames(result_df)) {
